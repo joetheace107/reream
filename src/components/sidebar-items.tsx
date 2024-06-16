b6bb044a6e8ce0abea143e95121723cc3437b431
joetheace107 <44982854+joetheace107@users.noreply.github.com>
2024-06-16 16:47:03 +0900

    feat: active nav items

---
+++ src/components/sidebar-items.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { Home, LineChart, Package, ShoppingCart } from "lucide-react";
+import Link from "next/link";
+import { useSelectedLayoutSegment } from "next/navigation";
+import { Badge } from "~/components/ui/badge";
+import { cn } from "~/lib/utils";
+
+export function SidebarItems() {
+  const segment = useSelectedLayoutSegment();
+
+  return (
+    <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+      <Link
+        href="/admin/"
+        className={cn(
+          "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+          !segment && "bg-muted text-primary",
+        )}
+      >
+        <Home className="size-4" />
+        Dashboard
+      </Link>
+      <Link
+        href="/admin/repos"
+        className={cn(
+          "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+          segment === "repos" && "bg-muted text-primary",
+        )}
+      >
+        <GitHubLogoIcon className="size-4" />
+        Repos
+      </Link>
+      <Link
+        href="/admin/deps"
+        className={cn(
+          "flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary",
+          segment === "deps" && "bg-muted text-primary",
+        )}
+      >
+        <Package className="size-4" />
+        Deps
+      </Link>
+      <Link
+        href="#"
+        className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+      >
+        <ShoppingCart className="size-4" />
+        Orders
+        <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
+          6
+        </Badge>
+      </Link>
+      <Link
+        href="#"
+        className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+      >
+        <LineChart className="size-4" />
+        Analytics
+      </Link>
+    </nav>
+  );
+}
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,14 +1,5 @@
-import {
-  Bell,
-  Home,
-  LineChart,
-  Package,
-  Package2,
-  ShoppingCart,
-  Users,
-} from "lucide-react";
+import { Bell, Package2 } from "lucide-react";
 import Link from "next/link";
-import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
 import {
   Card,
@@ -17,6 +8,7 @@ import {
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import { SidebarItems } from "./sidebar-items";
 
 export function Sidebar() {
   return (
@@ -33,46 +25,7 @@ export function Sidebar() {
           </Button>
         </div>
         <div className="flex-1">
-          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Home className="h-4 w-4" />
-              Dashboard
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <ShoppingCart className="h-4 w-4" />
-              Orders
-              <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                6
-              </Badge>
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg bg-muted px-3 py-2 text-primary transition-all hover:text-primary"
-            >
-              <Package className="h-4 w-4" />
-              Products{" "}
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <Users className="h-4 w-4" />
-              Customers
-            </Link>
-            <Link
-              href="#"
-              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
-            >
-              <LineChart className="h-4 w-4" />
-              Analytics
-            </Link>
-          </nav>
+          <SidebarItems />
         </div>
         <div className="mt-auto p-4">
           <Card x-chunk="dashboard-02-chunk-0">
