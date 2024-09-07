93d7e23eb6db0319a4a603e2d063564bfb826df8
joetheace107 <44982854+joetheace107@users.noreply.github.com>
2024-09-07 17:57:16 +0900

    feat: repo link

--- src/app/admin/repos/[repoId]/page.tsx
+++ src/app/admin/repos/[repoId]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import { db } from "~/db";
 
@@ -17,7 +18,12 @@ export default async function RepoPage({
     <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
       <div className="flex items-end justify-between">
         <h1 className="text-2xl font-semibold">
-          {repo.owner}/{repo.name}
+          <Link
+            href={`https://github.com/${repo.owner}/${repo.name}`}
+            target="_blank"
+          >
+            {repo.owner}/{repo.name}
+          </Link>
         </h1>
       </div>
     </main>
